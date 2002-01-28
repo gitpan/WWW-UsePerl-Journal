1581c7dee7d744538336fdca1e109f77f4b88931
Russell Matbouli <pause-spam@russell.matbouli.org>
2002-01-28 03:01:10 +0000

    Import of IDORU/WWW-UsePerl-Journal-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-UsePerl-Journal
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         IDORU/WWW-UsePerl-Journal-0.02.tar.gz
    gitpan-cpan-author:       IDORU
    gitpan-cpan-maturity:     released

--- Journal.pm
+++ Journal.pm
@@ -1,143 +1,288 @@
 package WWW::UsePerl::Journal;
 
-use strict;
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
-use LWP::Simple;
-
-require Exporter;
-
-@ISA = qw(Exporter);
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-@EXPORT = qw( );
-$VERSION = '0.01';
-
-
-# Preloaded methods go here.
-
-# Autoload methods go after =cut, and are processed by the autosplit program.
-
-sub new {
-    my $class = shift;
-    my $self  = bless {}, $class;
-    return $self;
-}#new
-  
-sub getUID {
-    my $self = shift;
-    my $user = shift;
-
-    print "getUID: user not stated\n" unless $user;
-    return unless $user;
-
-    my $content = get("http://use.perl.org/~$user/");
-    return unless $content;
-
-    $content =~ s/.*User info for $user \((\d+)\).*/$1/ism;
-    return unless $content;
-
-    return $content;
-}#getUID
-
-sub getEntryList {
-    my $self = shift;
-    my $user = shift;
-
-    my $UID = getUID($self, $user);
-    return unless $UID;
-
-    my $content = get("http://use.perl.org/journal.pl?op=list&uid=$UID");
-    return unless $content;
-    my @lines = split /\n/, $content;
-
-    my @entries = ();
-    foreach my $line (@lines){
-        next unless $line =~ /journal\.pl\?op=display&amp;uid=$UID/;
-        $line =~ 
-          s/.*?use\.perl\.org\/journal\.pl\?op=display
-          &amp;uid=$UID&amp;id=(\d*)"><b>(.*?)<\/b><\/a>.*
-          /$1, $2/ix;
-        $entries[$#entries+1] = $line;
-    }
-
-    return @entries;
-}#getEntryList
-
-sub getEntryIDs {
-    my $self = shift;
-    my $user = shift;
-
-    my @entries = getEntryList($self, $user);
-    my @IDs;
-
-    foreach (@entries) {
-    my ($id, $text) = split /, /;
-    $IDs[$#IDs+1] = $id;
-    }
-    return @IDs;
-}#getEntryIDs
-
-sub getEntryTitles {
-    my $self = shift;
-    my $user = shift;
-
-    my @entries = getEntryList($self, $user);
-    my @titles;
-
-    foreach (@entries) {
-    my ($id, $text) = split /, /;
-    $titles[$#titles+1] = $text;
-    }
-    return @titles;
-}#getEntryTitles
-
-sub getEntryByID {
-    my $self = shift;
-    my $user = shift;
-    my $ID = shift;
-
-    my $UID = getUID($self, $user);
-
-    my $content = get("http://use.perl.org/journal.pl?op=display&uid=$UID&id=$ID");
-    return "Sorry, this journal entry does not exist" 
-      if $content =~ 
-      m#Sorry, there are no journal entries found for this user.</TD></TR></TABLE><P>#;
-    $content =~ 
-      s#.*?$ID</a>\n]\n\s*</font>\n\s*<p>\n\s*(.*?)
-      \n\s*<br><br></div>.*#$1#ismx;
-    return $content;
-}#getEntryByID
-
-sub getEntryByTitle {
-    my $self = shift;
-    my $user = shift;
-    my $title = shift;
-    my @entries = getEntryList($self, $user);
-    my @ID = grep /$title/, @entries;
-    return unless defined $#ID;
-    my ($id, $d) = split /, /, $#ID;
-
-    return getEntryByID($self, $user, $id);
-}#getEntryByTitle
-
-1;
-__END__
 =head1 NAME
 
 WWW::UsePerl::Journal - use.perl.org journal tool
 
+=cut
+
 =head1 SYNOPSIS
 
   use WWW::UsePerl::Journal;
-  my $journal = WWW::UsePerl::Journal->new()
-  print $journal->getEntryByTitle("russell", "Text::Echelon");
-  my @entries = $journal->getEntryTitles("russell");
+  my $journal = WWW::UsePerl::Journal->new('russell')
+  print $journal->entrytitled("Text::Echelon");
+  my @entries = $journal->entrytitles();
+
+=cut
 
 =head1 DESCRIPTION
 
-Lists journal entries for a user. Can display a specific journal entry.
+Can list journal entries for a user.
+Can display a specific journal entry.
+Can post into a journal.
 
+=cut
+
+use strict;
+use vars qw($VERSION);
+use LWP::UserAgent;
+use HTTP::Cookies;
+use HTTP::Request::Common;
+use Data::Dumper;
+use Carp;
+
+
+use constant UP_URL => 'http://use.perl.org';
+
+my %postdefaults = (
+    topic     => 34,
+    comments  => 1,
+    type      => 1,
+);
+
+
+$VERSION = '0.02';
+
+=head2 new
+
+use WWW::UsePerl::Journal;
+my $j = WWW::UsePerl::Journal-E<gt>new('russell');
+
+Creates an instance for the specified user.
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $user  = shift or die "We need a user!";
+    my $ua = LWP::UserAgent->new(env_proxy => 1);
+    $ua->cookie_jar(HTTP::Cookies->new());
+    my $self  = bless { 
+        user => $user,
+        ua => $ua,
+        }, $class;
+    return $self;
+}#new
+
+=head2 user
+
+Returns the username
+
+=cut
+
+sub user { shift->{user} }
+
+=head2 uid
+
+Returns the user ID
+
+=cut
+
+sub uid {
+    my $self = shift;
+    $self->{_uid} ||= do {
+        my $user = $self->user;
+
+        my $content = $self->{ua}->request(GET UP_URL . "/~$user/")->content;
+        die "Cannot connect to " . UP_URL unless $content;
+
+        $content =~ m#User info for $user \((\d+)\)#ism
+          or die "$user does not exist";
+        $1;
+    }
+}#getUID
+
+=head2 entryhash
+
+Returns a hash of entry ID =E<gt> entry title
+
+=cut
+
+sub entryhash {
+    my $self = shift;
+    $self->{_entryhash} ||= do {
+        my $UID = $self->uid;
+        my $user = $self->user;
+
+        my $content = $self->{ua}->request(
+            GET UP_URL . "/journal.pl?op=list&uid=$UID")->content;
+        die "could not create entry list" unless $content;
+        my @lines = split /\n/, $content;
+
+        my %entries;
+        foreach my $line (@lines){
+            next unless $line =~ m#~$user/journal/#ism;
+            $line =~ m#~$user/journal/(\d*)/"><b>(.*?)</b></a>#ism;
+            next unless defined $1;
+            $entries{$1} = $2;
+        }
+
+    return %entries;
+    }
+}#entryhash
+
+=head2 entryids
+
+Returns an array of the entry IDs
+
+=cut
+
+sub entryids {
+    my $self = shift;
+    $self->{_entryids} ||= do {
+        my %entries = $self->entryhash;
+        my @IDs;
+
+        foreach (sort keys %entries) {
+        $IDs[$#IDs+1] = $_;
+        }
+        return @IDs;
+    }
+}#entryids
+
+=head2 entrytitles
+
+Returns an array of the entry titles
+
+=cut
+
+sub entrytitles {
+    my $self = shift;
+    $self->{_entrytitles} ||= do {
+        my %entries = $self->entryhash;
+        my @titles;
+
+        foreach (sort keys %entries) {
+            $titles[$#titles+1] = $entries{$_};
+        }
+        return @titles;
+    }
+}#entrytitles
+
+=head2 entry
+
+Returns the text of an entry, given an entry ID
+
+=cut
+
+sub entry {
+    my $self = shift;
+    my $ID = shift;
+    #$self->{_entry}{_$ID} ||= do {
+        my $UID = $self->uid;
+        my $content = $self->{ua}->request(
+            GET UP_URL . "/journal.pl?op=display&uid=$UID&id=$ID")->content;
+        die "error getting entry" unless $content;
+        die "$ID does not exist" 
+            if $content =~ 
+            m#Sorry, there are no journal entries 
+            found for this user.</TD></TR></TABLE><P>#ismx;
+        $content =~ 
+             m#.*?$ID</a>\n]\n\s*</font>\n\s*<p>\n\s*(.*?)
+             \n\s*<br><br></div>.*#ismx;
+        return $1;
+    #}
+}#entry
+
+=head2 entrytitled
+
+Returns the text of an entry, given an entry title
+
+=cut
+
+sub entrytitled {
+    my $self = shift;
+    my $title = shift;
+    my %entries = $self->entryhash;
+    foreach (sort keys %entries) {
+        next unless $entries{$_} =~ /$title/ism;
+        return $self->entry($_);
+    }
+    die "$title does not exist";
+}#entrytitled
+
+=head2 login
+
+Required before posting can occur, takes the password
+
+=cut
+
+sub login
+{
+    my ($self, $passwd) = (@_);
+    my $user = $self->user;
+    my $login =
+        $self->{ua}->request(
+           GET UP_URL . "/users.pl?op=userlogin&unickname=$user&upasswd=$passwd"
+        )->content;
+    die "login failed" unless $login;
+    return ( $login =~ /Welcome back $user/ism ) ? 1 : 0;
+}
+
+=head2 postentry
+
+Posts an entry into a journal, given a title and the text of the entry
+
+$j-E<gt>postentry({title =E<gt> "My journal is great", text =E<gt> "It really is"});
+
+=cut
+
+sub postentry
+{
+    my $self = shift;
+    my %params = (@_);
+    # Validate parameters
+    for (qw/text title/)
+    {
+    return undef unless exists $params{$_}
+    }
+    %params = (%postdefaults, %params);
+    $params{comments} = $params{comments} ? 1 : 0;
+    if ($params{type} !~ /^[1-4]$/)
+    {
+    die "Invalid post type.\n";
+    carp "Invalid post type.\n";
+    return undef;
+    }
+    if ($params{topic} !~ /^\d+$/)
+    {
+    die "Invalid journal topic.\n";
+    carp "Invalid journal topic.\n";
+    return undef;
+    }
+    if (length($params{title}) > 60)
+    {
+    die "Subject too long.\n";
+    carp "Subject too long.\n";
+    return undef;
+    }
+    # Post posting
+    my $editwindow =
+        $self->{ua}->request(
+        GET 'http://use.perl.org/journal.pl?op=edit')->content;
+    die "don't have an editwindow" unless $editwindow;
+    my ($formkey) = ($editwindow =~ m/formkey"\s+VALUE="([^"]+)"/ism);
+    die "No formkey!" unless defined $formkey;
+    croak "No formkey!" unless defined $formkey;
+    my %data = (
+    id  => '',
+    state   => 'editing',
+    preview => 'active',
+    formkey => $formkey,
+    description => $params{title},
+    tid => $params{topic},
+    journal_discuss => $params{comments},
+    article => $params{text},
+    posttype    => $params{type},
+    op  => 'save',
+    );
+    my $post = $self->{ua}->request(
+        POST 'http://use.perl.org/journal.pl', \%data);
+    return $post->is_success;
+}#postEntry
+
+1;
+__END__
 =head1 AVAILABILITY
 
 It should be available for download from
@@ -152,20 +297,20 @@ F<http://russell.matbouli.org/>
 
 =head1 TODO
 
-Optimise via caching - this will be in the next proper release I hope.
+Better error checking and test suite.
 
-Better error checking.
+Comment retrieval.
 
-Comment retrieval
-
-Writing activities (comments, create entry, modify, delete ...)
+Writing activities (comments, modify, delete ...)
 
 =head1 LICENSE
 
-Distributed under GPL v2. See COPYING included with this distibution.
+Distributed under GPL v2. See F<COPYING> included with this distibution.
 
 =head1 SEE ALSO
 
-http://use.perl.org/
+F<http://use.perl.org/>
+
+F<LWP>
 
 =cut
--- t/01test.t
+++ t/01test.t
@@ -1,48 +1,32 @@
 #!/usr/local/bin/perl -w
 
 use strict;
-use Test::More tests => 20;
+use Test::More tests => 8;
 
 use_ok('WWW::UsePerl::Journal');
 
-my $j = WWW::UsePerl::Journal->new();
-
+my $username = "russell";
+my $j = WWW::UsePerl::Journal->new($username);
 isa_ok($j, "WWW::UsePerl::Journal");
 
-# Test getUID
-my $username = "russell";
-my $UID = $j->getUID($username);
-isnt($UID, undef, "getUID returns defined");
-isnt($UID, "", "getUID doesn't return empty string");
-is($UID, 1413, "getUID gets the right ID");
+my $UID = $j->uid();
+is($UID, 1413, "uid");
 
-# Test getEntryList
-my @entries = $j->getEntryList($username);
-isnt($#entries, 0, "getEntryList got more than one entry");
-isnt($entries[0], undef, "getEntryList's first entry isn't undef");
-isnt($entries[0], "", "getEntryList's first entry isn't empty");
+my %entries = $j->entryhash;
+isnt(scalar %entries, 0, "entryhash");
 
-# Test getEntryIDs
-my @IDs = $j->getEntryIDs($username);
-isnt($#IDs, 0, "getEntryIDs got more than one entry");
-isnt($IDs[0], undef, "getEntryIDs first entry isn't undef");
-isnt($IDs[0], "", "getEntryIDs first entry isn't empty");
+my @IDs = $j->entryids;
+isnt(scalar @IDs, 0, "entryids");
 
-# Test getEntryTitles
-my @titles = $j->getEntryTitles($username);
-isnt($#titles, 0, "getEntryTitles got more than one entry");
-isnt($titles[0], undef, "getEntryTitles first entry isn't undef");
-isnt($titles[0], "", "getEntryTitles first entry isn't empty");
+my @titles = $j->entrytitles;
+isnt(scalar @titles, 0, "entrytitles");
 
-# Test getEntryByID
-my $EID = $j->getEntryByID($username, "2340");
-isnt($EID, undef, "getEntryByID returns defined");
-isnt($EID, "", "getEntryByID doesn't return empty string");
-is($EID, "I read in <A HREF=\"~hfb/journal/\">hfb's journal</A> that there was no module for testing whether something was a pangram. There is now.", "getEntryByID gets the right stuff");
+my $EID = $j->entry("2340");
+is($EID,
+"I read in <A HREF=\"~hfb/journal/\">hfb's journal</A> that there was no module for testing whether something was a pangram. There is now.",
+"entry");
 
-# Test getEntryByTitle
-my $text = $j->getEntryByTitle($username, "Lingua::Pangram");
-isnt($text, undef, "getEntryByTitle returns defined");
-isnt($text, "", "getEntryByTitle doesn't return empty string");
-is($text, "I read in <A HREF=\"~hfb/journal/\">hfb's journal</A> that there was no module for testing whether something was a pangram. There is now.", "getEntryByID gets the right stuff");
-
+my $text = $j->entrytitled("Lingua::Pangram");
+is($text,
+"I read in <A HREF=\"~hfb/journal/\">hfb's journal</A> that there was no module for testing whether something was a pangram. There is now.",
+"entrytitled");
